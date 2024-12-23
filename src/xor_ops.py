feeae8b30b4f1c7cc79c66e505d466915a7c19cc
liquidlegs <makurosofuto0uindo@gmail.com>
2024-12-23 15:13:04 +1030

    Fix a bug in the xor encryption algorithim

--- src/xor_ops.py
+++ src/xor_ops.py
@@ -2,7 +2,7 @@ def encrypt_string(data: str, key: str) -> str:
     if data == None or key == None:
         return None
     
-    output = [ord(char) ^ ord(key[i % 2]) for i, char in enumerate(data)]
+    output = [ord(char) ^ ord(key[i % len(key)]) for i, char in enumerate(data)]
     output = bytes(output).hex()
     return output
 
@@ -12,5 +12,5 @@ def decrypt_string(data: bytes, key: str) -> str:
         return None
     
     new_data = bytes.fromhex(data)
-    output = "".join([chr(nbr ^ ord(key[i % 2])) for i, nbr in enumerate(new_data)])
+    output = "".join([chr(nbr ^ ord(key[i % len(key)])) for i, nbr in enumerate(new_data)])
     return output
